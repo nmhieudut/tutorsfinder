62655dd012e2ecec929193e75d981708f4fced5f
tuanconbu <hieutk5@gmail.com>
2020-08-19 18:11:32 +0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+import Sider from './components/Sider'
 import './App.css';
+import { Layout,Menu, Switch } from 'antd';
+
+const { Header, Footer, Content } = Layout;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Layout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
---
+++ src/components/layouts/Sider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { Layout,Menu, Switch } from 'antd';
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
+
+export default function Sider() {
+    return (
+        <React.Fragment>
+            <Sider>Sider</Sider>
+        </React.Fragment>
+    )
+}
