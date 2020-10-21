aefb5a8b281b8acb92a8c06bbce86d786398b8e3
tuanconbu <hieutk5@gmail.com>
2020-10-22 00:53:17 +0700

    add helper

--- src/helper/decodeToken.js
+++ src/helper/decodeToken.js
@@ -1,6 +1,6 @@
 import jwt_decode from "jwt-decode";
 
-export const useToken = (token) => {
+export const decodeToken = (token) => {
   var decoded_token = jwt_decode(token);
   return decoded_token;
 };
