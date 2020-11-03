15aa550a2dc7f66da929f92ec00415560c145c30
tuanconbu <hieutk5@gmail.com>
2020-11-03 10:31:16 +0700

    up len chu ko mat

---
+++ src/features/main/components/Tutors/components/index.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import TutorsServices from "../../../../../api/TutorsServices";
+import { Table, Modal } from "antd";
+import "./tutors.modules.css";
+export default function TutorsComponents() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log("tutors", data);
+  useEffect(() => {
+    const fetchTutors = async () => {
+      setData(await TutorsServices.getAllTutors());
+      setLoading(false);
+    };
+    fetchTutors();
+  }, []);
+  function info(user) {
+    Modal.info({
+      title: "Detail",
+      content: (
+        <div className="detail-modal">
+          <p>Email: {user.email}</p>
+          <p>Gender : {user.gender ? "Male" : "Female"}</p>
+          <p>Phone: {user.phoneNumber}</p>
+          <p>Address: {user.address}</p>
+          <p>Introduction: {user.introduction}</p>
+        </div>
+      ),
+      onOk() {},
+    });
+  }
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "User",
+      dataIndex: "user",
+      key: "user",
+      render: (user) => (
+        <div>
+          {user.firstName + " " + user.lastName}{" "}
+          <a onClick={() => info(user)}>View Detail</a>
+        </div>
+      ),
+    },
+    {
+      title: "Efficency",
+      dataIndex: "efficency",
+      key: "efficency",
+    },
+    {
+      title: "Subject",
+      dataIndex: "subject",
+      key: "subject",
+      render: (subject) => (
+        <div>
+          {subject.length > 0
+            ? subject.map((item, index) => <p key={index}>{item}</p>)
+            : "No data"}
+        </div>
+      ),
+    },
+  ];
+  return (
+    <>
+      <Table
+        columns={columns}
+        bordered
+        loading={loading}
+        dataSource={data}
+        rowKey={(record) => `${record.id}`}
+      />
+    </>
+  );
+}
--- src/pages/tutors/Tutors.jsx
+++ src/pages/tutors/Tutors.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import TutorsComponents from "../../features/main/components/Tutors/components";
+import { Typography } from "antd";
 
+const { Title } = Typography;
 export default function Tutors() {
-  return <div>Tutors</div>;
+  return (
+    <div>
+      <div style={{ flex: 1, padding: "20px", textAlign: "left" }}>
+        <Title level={2}>Tutors</Title>
+      </div>
+      <TutorsComponents />
+    </div>
+  );
 }
